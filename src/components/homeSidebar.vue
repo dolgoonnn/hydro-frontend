15f9aa8dad83f5c3850f83ec3a4bac795b6b0504
Jurjma <enkhmaty@gmail.com>
2022-06-10 00:47:29 +0800

    category

--- src/components/homeSidebar.vue
+++ src/components/homeSidebar.vue
@@ -102,7 +102,91 @@
                               C255,161.018,253.42,157.202,250.606,154.389z"
                 />
               </svg>
+              <span class="ml-3">Холбооны дүрэм</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
+            >
+              <svg
+                class="w-4 h-4 flex-shrink-0"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_222_"
+                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+                              C255,161.018,253.42,157.202,250.606,154.389z"
+                />
+              </svg>
               <span class="ml-3">Гишүүнчлэл</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
+            >
+              <svg
+                class="w-4 h-4 flex-shrink-0"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_222_"
+                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+                              C255,161.018,253.42,157.202,250.606,154.389z"
+                />
+              </svg>
+              <span class="ml-3">Хууль</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
+            >
+              <svg
+                class="w-4 h-4 flex-shrink-0"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_222_"
+                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+                              C255,161.018,253.42,157.202,250.606,154.389z"
+                />
+              </svg>
+              <span class="ml-3">Журам</span>
             </a>
           </li>
           <li>
@@ -158,91 +242,7 @@
                               C255,161.018,253.42,157.202,250.606,154.389z"
                 />
               </svg>
-              <span class="ml-3">Хууль дүрэм</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
-            >
-              <svg
-                class="w-4 h-4 flex-shrink-0"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 330 330"
-                style="enable-background: new 0 0 330 330"
-                xml:space="preserve"
-              >
-                <path
-                  id="XMLID_222_"
-                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
-                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
-                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
-                              C255,161.018,253.42,157.202,250.606,154.389z"
-                />
-              </svg>
               <span class="ml-3">Мэдээ</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
-            >
-              <svg
-                class="w-4 h-4 flex-shrink-0"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 330 330"
-                style="enable-background: new 0 0 330 330"
-                xml:space="preserve"
-              >
-                <path
-                  id="XMLID_222_"
-                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
-                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
-                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
-                              C255,161.018,253.42,157.202,250.606,154.389z"
-                />
-              </svg>
-              <span class="ml-3">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:text-ccblue hover:fill-ccblue"
-            >
-              <svg
-                class="w-4 h-4 flex-shrink-0"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 330 330"
-                style="enable-background: new 0 0 330 330"
-                xml:space="preserve"
-              >
-                <path
-                  id="XMLID_222_"
-                  d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
-                              c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
-                              C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
-                              C255,161.018,253.42,157.202,250.606,154.389z"
-                />
-              </svg>
-              <span class="ml-3">Dashboard</span>
             </a>
           </li>
         </ul>
